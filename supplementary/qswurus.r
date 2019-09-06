dbf5682e2597a8505aa6f60aba3821da9d6c3baf
Jason Cory Brunson <cornelioid@gmail.com>
2019-09-06 13:41:16 -0400

    track supplementary folder + change untracked folder from sandbox to ignore

---
+++ supplementary/qswurus.r
@@ -0,0 +1,54 @@
+library(magrittr)
+library(readxl)
+library(dplyr)
+
+names_qs <- c(
+  "institution", "country", "size", "focus", "res", "age", "status",
+  "rk_academic", "rk_employer",
+  "rk_ratio", "rk_citations",
+  "rk_intl_faculty", "rk_intl_students"
+)
+
+read_qs1 <- function(path, year) {
+  path %>%
+    read_xlsx(sheet = 1, skip = 3) %>%
+    select(7, 8, 9:13, seq(15, 25, 2)) %>%
+    set_names(names_qs) %>%
+    mutate(year = year)
+}
+
+read_qs1("~/Downloads/QS World University Rankings 2016-2017.xlsx", 2017L)
+
+read_qs2 <- function(path, year) {
+  path %>%
+    read_xlsx(sheet = 1, skip = 4) %>%
+    select(3, 4, 5:9, seq(11, 21, 2)) %>%
+    set_names(names_qs) %>%
+    mutate(year = year)
+}
+
+read_qs2("~/Downloads/2018-QS-World-University-Rankings-v1.1.1.xlsx", 2018L)
+read_qs2("~/Downloads/2019-QS-World-University-Rankings-v1.0.xlsx", 2019L)
+read_qs2("~/Downloads/2020-QS-World-University-Rankings-v1.0.xlsx", 2020L)
+
+qswur <- bind_rows(
+  read_qs1("~/Downloads/QS World University Rankings 2016-2017.xlsx", 2017L),
+  read_qs2("~/Downloads/2018-QS-World-University-Rankings-v1.1.1.xlsx", 2018L),
+  read_qs2("~/Downloads/2019-QS-World-University-Rankings-v1.0.xlsx", 2019L),
+  read_qs2("~/Downloads/2020-QS-World-University-Rankings-v1.0.xlsx", 2020L)
+)
+qswur %>%
+  filter(country %in% c("US", "United States")) %>%
+  count(year)
+
+qswur %>%
+  filter(year == 2020, country %in% c("US", "United States")) %>%
+  select(-country) %>%
+  mutate_at(vars(starts_with("rk_")), as.integer) %>%
+  tidyr::drop_na() %>%
+  mutate(institution = toupper(institution)) %>%
+  add_count(institution) %>%
+  filter(n == max(n)) %>%
+  select(year, everything(), -n) %>%
+  print() -> qswurus20
+readr::write_rds(qswurus20, here::here("sandbox/qswurus20.rds"))
---
+++ supplementary/topics.md
@@ -0,0 +1,43 @@
+---
+draft: false
+title: Topics
+---
+
+## professional development
+
+- essay for RCR course
+- open access
+- reviewing (transparency, currency / incentive structure of each culture / ecosystem)
+- my favorite theorem
+
+## research progress
+
+- scientometrics: predicting impact versus understanding forces / incentives
+- MIMIC-III code
+- fixed versus random effects
+- instantiating the MIMIC-III database using **dbplyr**?
+- "nice" 3-layered / depth-3 covers of the plane
+- [power laws post](https://rpubs.com/CoryBrunson/proteome-powerlaw)
+
+## methodological technique
+
+- survey of clustering coefficients
+- calculating tables of Kendall's tau
+- circuit analysis--based graph layout for birooted graphs
+- correspondence analysis of Du Bois data on black families
+- binomial regression of a Poisson random variable
+- bipartite configuration random graph models
+- angle distance between PCAs on corresponding point clouds
+
+## personal curiosity
+
+- tidytuesday
+- "Hidden Figures"
+- polyamory books on Goodreads
+- CT state funding of UConn & UConn Health
+- journalism subscriptions
+- charitable donations
+- analysis of shiggy trails
+- alluvial diagram of CT tiered beaches
+- factor analysis of ecopanel data
+- tokusatsu collaboration network
